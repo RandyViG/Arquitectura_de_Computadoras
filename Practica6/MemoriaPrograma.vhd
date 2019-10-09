b90a003d759cdcf8eae70f4b77ee001874d61336
Randy Villanueva <rvillanuevag1603@alumno.ipn.mx>
2019-10-08 20:02:19 -0500

    Agregando todo el set de instrucciones del ESCOMips

--- Practica6/MemoriaPrograma.vhd
+++ Practica6/MemoriaPrograma.vhd
@@ -15,40 +15,88 @@ entity MemoriaPrograma is
 end MemoriaPrograma;
 
 architecture Behavioral of MemoriaPrograma is
-constant op_li : std_logic_vector(4 downto 0) := "00001";
-constant op_bgti : std_logic_vector(4 downto 0):="10001";
-constant op_swi : std_logic_vector(4 downto 0):="00011";
-constant op_b : std_logic_vector(4 downto 0):="10011";
-constant op_nop: std_logic_vector(4 downto 0):="10110";
 
-constant r0 : std_logic_vector(3 downto 0) := "0000";
-constant r1 : std_logic_vector(3 downto 0) := "0001";
-constant r2 : std_logic_vector(3 downto 0) := "0010";
-
-constant su : std_logic_vector(3 downto 0) := "0000";
+--CONSTANTES DE INSTRUCCIONES PARA EL ESCOMips
+--Instrucciones tipo R
+constant tipo_r : std_logic_vector (4 downto 0) := "00000";
+--Carga y Almacenamiento
+constant li : std_logic_vector (4 downto 0) := "00001";
+constant lwi : std_logic_vector (4 downto 0) := "00010";
+constant lw : std_logic_vector (4 downto 0) := "10111";
+constant swi : std_logic_vector (4 downto 0) := "00011";
+constant sw : std_logic_vector (4 downto 0) := "00100";
+--Aritmticas
+constant addi : std_logic_vector (4 downto 0) := "00101";
+constant subi : std_logic_vector (4 downto 0) := "00110";
+--Identificador Aritmticas R
+constant add : std_logic_vector (3 downto 0) := "0000";
+constant sub : std_logic_vector (3 downto 0) := "0001";
+--Logicas
+constant andi : std_logic_vector (4 downto 0) := "00111";
+constant ori : std_logic_vector (4 downto 0) := "01000";
+constant xori : std_logic_vector (4 downto 0) := "01001";
+constant nandi : std_logic_vector (4 downto 0) := "01010";
+constant nori : std_logic_vector (4 downto 0) := "01011";
+constant xnori : std_logic_vector (4 downto 0) := "01100";
+--Identificador Logicas R
+constant andr : std_logic_vector (3 downto 0) := "0010";
+constant orr : std_logic_vector (3 downto 0) := "0011";
+constant xorr : std_logic_vector (3 downto 0) := "0100";
+constant nandr : std_logic_vector (3 downto 0) := "0101";
+constant norr : std_logic_vector (3 downto 0) := "0110";
+constant xnorr : std_logic_vector (3 downto 0) := "0111";
+constant notr : std_logic_vector (3 downto 0) := "1000";
+--Identificador Corrimiento R
+constant sllr : std_logic_vector (3 downto 0) := "1001";
+constant srlr : std_logic_vector (3 downto 0) := "1010";
+constant dos : std_logic_vector (3 downto 0) := "0010";
+--Saltos Condicionales e Incondicionales
+constant beqi : std_logic_vector (4 downto 0) := "01101";
+constant bnei : std_logic_vector (4 downto 0) := "01110";
+constant blti : std_logic_vector (4 downto 0) := "01111";
+constant bleti : std_logic_vector (4 downto 0) := "10000";
+constant bgti : std_logic_vector (4 downto 0) := "10001";
+constant bgeti : std_logic_vector (4 downto 0) := "10010";
+constant b : std_logic_vector (4 downto 0) := "10011";
+--Manejo de Subrutinas
+constant call : std_logic_vector (4 downto 0) := "10100";
+constant ret : std_logic_vector (4 downto 0) := "10101";
+--Otros
+constant nop : std_logic_vector (4 downto 0) := "10110";
+constant opr : std_logic_vector ( 4 downto 0) := "00000";
+constant su : std_logic_vector (3 downto 0) := "0000"; -- sin usar
+--Registros
+constant R0 : std_logic_vector (3 downto 0) := "0000";
+constant R1 : std_logic_vector (3 downto 0) := "0001";
+constant R2 : std_logic_vector (3 downto 0) := "0010";
+constant R3 : std_logic_vector (3 downto 0) := "0011";
+constant R4 : std_logic_vector (3 downto 0) := "0100";
+constant R5 : std_logic_vector (3 downto 0) := "0101";
+constant R6 : std_logic_vector (3 downto 0) := "0110";
+constant R7 : std_logic_vector (3 downto 0) := "0111";
+constant R8 : std_logic_vector (3 downto 0) := "1000";
+constant R9 : std_logic_vector (3 downto 0) := "1001";
+constant R10 : std_logic_vector (3 downto 0) := "1010";
+constant R11 : std_logic_vector (3 downto 0) := "1011";
+constant R12 : std_logic_vector (3 downto 0) := "1100";
+constant R13 : std_logic_vector (3 downto 0) := "1101";
+constant R14 : std_logic_vector (3 downto 0) := "1110";
+constant R15 : std_logic_vector (3 downto 0) := "1111";
 
 type arreglo is array ( 0 to (2**bus_datos)-1 ) of std_logic_vector(24 downto 0);
 constant aux : arreglo := (
-op_li & r0 & x"0023",       --li r0, 23
-op_li & r1 & x"FFd3",       -- li r1,-45
-op_li & r2 & x"0165",       --li r2, 165
-op_bgti & r1 & r0 & x"006", --BGTI R1,R0,5
-op_bgti & r2 & r1 & x"003", --BGTI R2,R1,3
-op_swi & r2 & x"0020",      --SWI, R2, 0x20
-op_b & su & x"000D",        --B PC = 13
-op_swi & r1 & x"0020",      --SWI, R1, 0x20
-op_b & su & x"000D",        --B PC = 13
-op_bgti & r2 & r0 & x"003", --BGTI R2,R0,3
-op_swi & r2 & x"0020",      --SWI, R2, 0x20
-op_b & su & x"000D",        --B PC = 13
-op_swi & r0 & x"0020",      --SWI, R0, 0x20
-op_nop & su & su &su & su & su, --NOP
-op_b & su & x"000D",        --B PC = 13
+opr & r8 &r4 &r9& su& add,
+call & su & x"0007",
+sw & R8 & R4 & x"048",
+subi & R5 & R6 & x"721",
+bnei & r3 & r2 & x"FFE",
+opr&r6&r3&su&x"3"&srlr,
+
 others => (others => '0')
 );
 begin
 
-	dout <= aux(conv_integer(dir));
+    dout <= aux(conv_integer(dir));
 
 end Behavioral;
 
