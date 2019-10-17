df2ab33a1f7a3c08a3d4bdd2bf81311082a5717c
RandyViG <rvillanuevag1603@alumno.ipn.mx>
2019-10-16 22:16:19 -0500

    Acomodando codigo

--- Practica8/Fetch.vhd
+++ Practica8/Fetch.vhd
@@ -3,15 +3,15 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
 entity Fetch is
-    Port ( pcin : in  STD_LOGIC_VECTOR (15 downto 0);
-           wpc,up,dw,clr,clk : in  STD_LOGIC;
-			  sp : out STD_LOGIC_VECTOR (2 downto 0);
-           dataout : out  STD_LOGIC_VECTOR (24 downto 0));
+    Port ( 
+        pcin : in  STD_LOGIC_VECTOR (15 downto 0);
+        wpc,up,dw,clr,clk : in  STD_LOGIC;
+        sp : out STD_LOGIC_VECTOR (2 downto 0);
+        dataout : out  STD_LOGIC_VECTOR (24 downto 0));
 end Fetch;
 
 architecture Behavioral of Fetch is
-	
-	component MemoriaPrograma is
+    component MemoriaPrograma is
     generic(
         bus_datos: Integer:=16;
         instruccion : Integer :=25
@@ -19,34 +19,34 @@ architecture Behavioral of Fetch is
     Port ( 
         dir : in  STD_LOGIC_VECTOR (bus_datos-1 downto 0);
         dout : out  STD_LOGIC_VECTOR (instruccion-1 downto 0));
-	end component;
-	
-	component pila is
+    end component;
+    
+    component pila is
     Port ( 
         clk, clr, up, dw, wpc : in  STD_LOGIC;
         sp1 : out std_logic_vector(2 downto 0);
         pcin : in  STD_LOGIC_VECTOR (15 downto 0);
         pcout : out  STD_LOGIC_VECTOR (15 downto 0)
     );
-	end component;
-	
-	signal aux_pcout : std_logic_vector (15 downto 0);
+    end component;
+    
+    signal aux_pcout : std_logic_vector (15 downto 0);
 begin
 
-	stack: pila Port map(
-		clr => clr,
-		clk => clk,
-		up => up,
-		dw => dw,
-		wpc => wpc,
-		pcin => pcin,
-		pcout => aux_pcout,
-		sp1 => sp
-	); 
-	memoria: MemoriaPrograma Port map(
-		dir => aux_pcout,
-		dout => dataout
-	);
+    stack: pila Port map(
+        clr => clr,
+        clk => clk,
+        up => up,
+        dw => dw,
+        wpc => wpc,
+        pcin => pcin,
+        pcout => aux_pcout,
+        sp1 => sp
+    ); 
+    memoria: MemoriaPrograma Port map(
+        dir => aux_pcout,
+        dout => dataout
+    );
 
 end Behavioral;
 
--- Practica8/tb_fetch.vhd
+++ Practica8/tb_fetch.vhd
@@ -15,142 +15,121 @@ ARCHITECTURE behavior OF tb_fetch IS
     -- Component Declaration for the Unit Under Test (UUT)
     COMPONENT Fetch
     PORT(
-         pcin : IN  std_logic_vector(15 downto 0);
-         wpc : IN  std_logic;
-         up : IN  std_logic;
-         dw : IN  std_logic;
-         clr : IN  std_logic;
-         clk : IN  std_logic;
-			sp : OUT std_logic_vector(2 downto 0);
-         dataout : OUT  std_logic_vector(24 downto 0)
+        pcin : IN  std_logic_vector(15 downto 0);
+        wpc : IN  std_logic;
+        up : IN  std_logic;
+        dw : IN  std_logic;
+        clr : IN  std_logic;
+        clk : IN  std_logic;
+        sp : OUT std_logic_vector(2 downto 0);
+        dataout : OUT  std_logic_vector(24 downto 0)
         );
     END COMPONENT;
-	 
+    
    --Inputs
-   signal pcin : std_logic_vector(15 downto 0) := (others => '0');
-   signal wpc : std_logic := '0';
-   signal up : std_logic := '0';
-   signal dw : std_logic := '0';
-   signal clr : std_logic := '0';
-   signal clk : std_logic := '0';
+    signal pcin : std_logic_vector(15 downto 0) := (others => '0');
+    signal wpc : std_logic := '0';
+    signal up : std_logic := '0';
+    signal dw : std_logic := '0';
+    signal clr : std_logic := '0';
+    signal clk : std_logic := '0';
 
- 	--Outputs
-   signal dataout : std_logic_vector(24 downto 0);
-	signal sp : std_logic_vector(2 downto 0) := (others => '0');
+    --Outputs
+    signal dataout : std_logic_vector(24 downto 0);
+    signal sp : std_logic_vector(2 downto 0) := (others => '0');
 
-   -- Clock period definitions
-   constant clk_period : time := 10 ns;
+    -- Clock period definitions
+    constant clk_period : time := 10 ns;
  
 BEGIN
  
-	-- Instantiate the Unit Under Test (UUT)
-   uut: Fetch PORT MAP (
-          pcin => pcin,
-          wpc => wpc,
-          up => up,
-          dw => dw,
-          clr => clr,
-          clk => clk,
-			 sp => sp,
-          dataout => dataout
-        );
+    -- Instantiate the Unit Under Test (UUT)
+    uut: Fetch PORT MAP (
+        pcin => pcin,
+        wpc => wpc,
+        up => up,
+        dw => dw,
+        clr => clr,
+        clk => clk,
+        sp => sp,
+        dataout => dataout
+    );
 
    -- Clock process definitions
    clk_process :process
    begin
-		clk <= '0';
-		wait for clk_period/2;
-		clk <= '1';
-		wait for clk_period/2;
+    clk <= '0';
+    wait for clk_period/2;
+    clk <= '1';
+    wait for clk_period/2;
    end process;
  
    -- Stimulus process
    stim_proc: process
-	file ARCH_RES : TEXT;	--Apuntadores tipo txt
+    file ARCH_RES : TEXT;	--Apuntadores tipo txt
     variable LINEA_RES : line;
    begin	
-		file_open(ARCH_RES, "Resultado.txt", WRITE_MODE);
-		wait for 10 ns;
-		
-		pcin<=x"0000"; 
-      wpc<='0';
-      up<='0';
-      dw<='0';
-      clr<='1';
-		wait for 10 ns;
-		write(LINEA_RES, dataout, left, 6);--ESCRIBE EL CAMPO dataout
-		writeline(ARCH_RES, LINEA_RES);
-		
-		
-		pcin<=x"0000"; 
-      wpc<='0';
-      up<='0';
-      dw<='0';
-      clr<='0';
-		wait for 10 ns;
-		write(LINEA_RES, dataout, left, 6);--ESCRIBE EL CAMPO dataout
-		writeline(ARCH_RES, LINEA_RES);
-		
-		wait for 10 ns;
-		write(LINEA_RES, dataout, left, 6);--ESCRIBE EL CAMPO dataout
-		writeline(ARCH_RES, LINEA_RES);
-		
-		
-		for i in 0 to 9 loop
-		pcin<=x"0005"; 
-      wpc<='1';
-      up<='1';
-      dw<='0';
-      clr<='0';
-		wait for 10 ns;
-		write(LINEA_RES, dataout, left, 6);--ESCRIBE EL CAMPO dataout
-		writeline(ARCH_RES, LINEA_RES);
-		
-		
-		pcin<=x"0000"; 
-      wpc<='0';
-      up<='0';
-      dw<='0';
-      clr<='0';
-		wait for 10 ns;
-		write(LINEA_RES, dataout, left, 6);--ESCRIBE EL CAMPO dataout
-		writeline(ARCH_RES, LINEA_RES);
-		
-      
-		pcin<=x"0003"; 
-      wpc<='0';
-      up<='0';
-      dw<='1';
-      clr<='0';
-		wait for 10 ns;
-		write(LINEA_RES, dataout, left, 6);--ESCRIBE EL CAMPO dataout
-		writeline(ARCH_RES, LINEA_RES);
-		
-		
-		pcin<=x"0000"; 
-      wpc<='0';
-      up<='0';
-      dw<='0';
-      clr<='0';
-		wait for 10 ns;
-		write(LINEA_RES, dataout, left, 6);--ESCRIBE EL CAMPO dataout
-		writeline(ARCH_RES, LINEA_RES);
-		
-		
-		pcin<=x"0002"; 
-      wpc<='1';
-      up<='0';
-      dw<='0';
-      clr<='0';
-		wait for 10 ns;
-		write(LINEA_RES, dataout, left, 6);--ESCRIBE EL CAMPO dataout
-		writeline(ARCH_RES, LINEA_RES);
-		
-		end loop;
-		
-		file_close(ARCH_RES);  -- cierra el archivo
-		wait;
-		
+    file_open(ARCH_RES, "Resultado.txt", WRITE_MODE);
+    wait for 10 ns;
+    
+    pcin<=x"0000"; 
+    wpc<='0';
+    up<='0';
+    dw<='0';
+    clr<='1';
+    wait for 10 ns;
+    write(LINEA_RES, dataout, left, 6);--ESCRIBE EL CAMPO dataout
+    writeline(ARCH_RES, LINEA_RES);
+    
+    pcin<=x"0000"; 
+    clr<='0';
+    wait for 10 ns;
+    write(LINEA_RES, dataout, left, 6);--ESCRIBE EL CAMPO dataout
+    writeline(ARCH_RES, LINEA_RES);
+    
+    wait for 10 ns;
+    write(LINEA_RES, dataout, left, 6);--ESCRIBE EL CAMPO dataout
+    writeline(ARCH_RES, LINEA_RES);
+    
+    for i in 0 to 9 loop
+    pcin<=x"0005"; 
+    wpc<='1';
+    up<='1';
+    wait for 10 ns;
+    write(LINEA_RES, dataout, left, 6);--ESCRIBE EL CAMPO dataout
+    writeline(ARCH_RES, LINEA_RES);
+    
+    pcin<=x"0000";
+    wpc<='0';
+    up<='0';
+    wait for 10 ns;
+    write(LINEA_RES, dataout, left, 6);--ESCRIBE EL CAMPO dataout
+    writeline(ARCH_RES, LINEA_RES);
+        
+    pcin<=x"0003"; 
+    dw<='1';
+    wait for 10 ns;
+    write(LINEA_RES, dataout, left, 6);--ESCRIBE EL CAMPO dataout
+    writeline(ARCH_RES, LINEA_RES);
+    
+    pcin<=x"0000"; 
+    dw<='0';
+
+    wait for 10 ns;
+    write(LINEA_RES, dataout, left, 6);--ESCRIBE EL CAMPO dataout
+    writeline(ARCH_RES, LINEA_RES);
+    
+    pcin<=x"0002"; 
+    wpc<='1';
+    wait for 10 ns;
+    write(LINEA_RES, dataout, left, 6);--ESCRIBE EL CAMPO dataout
+    writeline(ARCH_RES, LINEA_RES);
+    
+    end loop;
+    
+    file_close(ARCH_RES);  -- cierra el archivo
+    wait;
+    
    end process;
 
 END;
